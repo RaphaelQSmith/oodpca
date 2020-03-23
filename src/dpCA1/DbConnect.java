c5f09336e91eedfaac09b95e883baf100a5233c7
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2020-03-23 12:23:34 +0000

    "First Commit"

---
+++ src/dpCA1/DbConnect.java
@@ -0,0 +1,38 @@
+package dpCA1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DbConnect {
+	
+	public DbConnect() {
+		
+	}
+
+	@SuppressWarnings("deprecation")
+	public void connectDB() {
+		
+		try{
+			// Load the database driver
+			Class.forName("com.mysql.jdbc.Driver").newInstance() ;
+			
+			String dbServer = "jdbc:mysql://52.50.23.197:3306/world";
+			String user = "cctstudent";
+			String password = "Pass1234!";
+			String query = "SELECT * FROM country";
+		
+			// Get a connection to the database
+			Connection conn = DriverManager.getConnection(dbServer, user, password);
+			
+			Statement stmt=conn.createStatement();  
+			ResultSet rs=stmt.executeQuery(query);  
+		
+			while(rs.next())  
+				System.out.println(rs.getObject(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+				conn.close();  
+				}catch(Exception e){ System.out.println(e);}  
+				}  
+					
+	}
+
---
+++ src/dpCA1/Test.java
@@ -0,0 +1,13 @@
+package dpCA1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DbConnect db = new DbConnect();
+		db.connectDB();
+		
+	}
+
+}
